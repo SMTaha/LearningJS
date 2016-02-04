255d51dfe8e25e09115e90159706d18011d61a15
Syed M. Taha <s.m.taha10@gmail.com>
2016-02-04 22:53:11 +0500

    Add images

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ function checkSubmission() {
     if (email === "") {
         document.getElementById("heading").className = "hidden";
     } else {
-        document.getElementsByTagName("h1").className = "hidden";
+        document.getElementsByTagName("h1").className += " hidden";
     }
 }
 
 function makeInvisible() {
-    document.getElementById("heading").className = "hidden";
+    document.getElementById("heading").className += " hidden";
 }
 function focused(){
     document.getElementById("submit").className = "hidden";
+}
+
+function swapPic(id, image){
+    document.getElementById(id).src = image;
+    document.getElementById(id).style.cssFloat = 'left';
 }
