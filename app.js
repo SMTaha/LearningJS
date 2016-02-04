5a27ac7fb258512eaa41f95ce30a71e63b1b2054
Syed M. Taha <s.m.taha10@gmail.com>
2016-02-04 22:05:36 +0500

    Working submit button

--- app.js
+++ app.js
@@ -4,7 +4,16 @@
 
 function checkSubmission() {
     var email = document.getElementById("email").value;
-    if (email.length === 0) {
-        alert("Email is required.");
+    if (email === "") {
+        document.getElementById("heading").className = "hidden";
+    } else {
+        document.getElementsByTagName("h1").className = "hidden";
     }
+}
+
+function makeInvisible() {
+    document.getElementById("heading").className = "hidden";
+}
+function focused(){
+    document.getElementById("submit").className = "hidden";
 }
